e4125a863837eccb4fc31bc3ab12f44d5d29b89f
Trung Em <nguyentrungem2002@gmail.com>
2023-03-19 11:50:16 +0700

    Làm phần courses

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
                 "4xl": "0px 22px 50px rgba(0, 0, 0, 0.05)",
                 "5xl": "0px 10px 80px rgba(0, 0, 0, 0.05)",
                 "6xl": "0px 12px 80px rgba(0, 0, 0, 0.05)",
+                "7xl": "0px 16px 90px rgba(0, 0, 0, 0.05)",
             },
         },
     },
